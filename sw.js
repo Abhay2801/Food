58bc2e78ba849056b56f839b9af9061e6b86db73
Abhishek Gupta <87697058+Abhay2801@users.noreply.github.com>
2022-04-08 15:34:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -101,3 +101,32 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', () => {
     console.log("sw is Activated")
 })
+
+
+
+var cacheName = 'geeks-cache-v1';
+
+// Call Fetch Event
+self.addEventListener('fetch', e => {
+    console.log('Service Worker: Fetching');
+    e.respondWith(
+        fetch(e.request)
+        .then(res => {
+            // The response is a stream and in order the browser
+            // to consume the response and in the same time the
+            // cache consuming the response it needs to be
+            // cloned in order to have two streams.
+            const resClone = res.clone();
+            // Open cache
+            caches.open(cacheName)
+                .then(cache => {
+                    // Add response to cache
+                    cache.put(e.request, resClone);
+                });
+            return res;
+        }).catch(
+            err => caches.match(e.request)
+            .then(res => res)
+        )
+    );
+});
